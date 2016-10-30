a160b184f36e631fd26f8b3d2f8a4e4d8a2d08ae
CJ <thecjhorton@gmail.com>
2016-10-29 21:12:17 -0500

    add login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,4 +37,27 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/login' do
+      if logged_in
+        redirect to '/jobs'
+      else
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          redirect to '/jobs'
+        else
+          erb :error
+        end
+      end
+  end
+
+  get '/logout' do
+    if logged_in
+      session.clear
+      redirect '/'
+    else
+      redirect '/'
+    end
+  end
+
 end
